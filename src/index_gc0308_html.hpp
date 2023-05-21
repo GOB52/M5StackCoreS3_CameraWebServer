142d434eaa0d00b87c3e8b7adc613d53d90d9ead
GOB <goblin52@gmail.com>
2023-05-21 15:53:18 +0900

    Add support face detection and recognition

--- src/index_gc0308_html.hpp
+++ src/index_gc0308_html.hpp
@@ -1,5 +1,12 @@
 #ifndef INDEX_GC0308_HTML_HPP
 #define INDEX_GC0308_HTML_HPP
+
+#if defined(CWS_ENABLE_FACE)
+extern unsigned char index_gc0308_face_html_gz[];
+extern unsigned int index_gc0308_face_html_gz_len;
+#else
 extern unsigned char index_gc0308_html_gz[];
 extern unsigned int index_gc0308_html_gz_len;
+#endif
+
 #endif
